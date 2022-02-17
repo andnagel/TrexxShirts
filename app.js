9bee0a29c97de402a6088135484e3ae88999f891
DroN <87648983+andnagel@users.noreply.github.com>
2022-02-17 14:17:54 +0100

    routing deluxe implementiert :-)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const db = require('./models')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var customersRouter = require('./routes/customers');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/customers', customersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users', { title: 'Users'});
+router.get('/', function(req, res) {
+  res.render('users.pug', { title: 'Users'});
 });
+
+router.get('/new', function(req, res) {
+  res.render('newUser.pug', { title: 'Users'});
+});
+
+router.post('/create', function(req, res) {
+  console.log('It worked')
+});
+
+// router.post('/create', (req, res) => {
+//   console.log('It worked')
+//   res.end('Irgendwas')
+// });
 
 module.exports = router;

7f24bdf47d2aba5dfd07cc82abd7d6270066d0f2
DroN <87648983+andnagel@users.noreply.github.com>
2022-02-18 09:01:48 +0100

    nodemon added, routing refactored

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models')
 
 
 router.get('/', function(req, res) {
+  //db.Customer.findAll().then(customers => res.send(customers));
   res.render('customers.pug', { title: 'Kunden'});
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models')
 
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('users.pug', { title: 'Users'});
+  db.User.findAll().then(users => res.send(users));
+  // res.render('users.pug', { title: 'Users'});
 });
 
 router.get('/new', function(req, res) {
   res.render('newUser.pug', { title: 'Users'});
 });
 
-router.post('/create', function(req, res) {
-  console.log('It worked')
+router.get('/find/:id', (req, res) => {
+  db.User.findAll({
+    where: {
+      id: req.params.id
+    }
+  }).then(user => res.send(user));
 });
+
+router.post('/create', (req, res) => {
+  console.log('It worked')
+  db.User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password
+  }).then(submittedUser => res.send(submittedUser));
+});
+
+router.delete('/delete/:id', (req, res) => {
+  db.User.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(() => res.send('success'));
+});
+
+router.put('/edit', (req, res) => {
+  
+});
+
+
+
+
 
 // router.post('/create', (req, res) => {
 //   console.log('It worked')

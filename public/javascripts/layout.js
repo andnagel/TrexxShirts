483985995f3243f5aff955d45ecc5e158fc5005c
DroN <87648983+andnagel@users.noreply.github.com>
2022-02-19 14:04:55 +0100

    search by id and added, clicking in the list view on a specific customer also navigates you to details now

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -3,6 +3,7 @@ const btnSearchCustomer = document.getElementById("btnSearchCustomer");
 
 btnSearchCustomer.addEventListener("click", () => {
     const id = customerId.value;
-    console.log(id)
-    // navigate to details view with this value
+    console.log(id);
+    // navigate to details view 
+    location.href = `/customers/details/${id}`;
 });

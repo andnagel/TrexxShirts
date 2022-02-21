9ebfc36d26165a932282c0a9fd986ad5b86a8cd6
DroN <87648983+andnagel@users.noreply.github.com>
2022-02-21 08:44:03 +0100

    customer validation added

--- public/javascripts/new.js
+++ public/javascripts/new.js
@@ -1,33 +1,47 @@
 const firstName = document.getElementById("name");
+const errorFirstName = document.getElementById("errorFirstName");
 const lastName = document.getElementById("lastName");
+const errorLastName = document.getElementById("errorLastName");
 const email = document.getElementById("email");
+const errorEmail = document.getElementById("errorEmail");
 const birthdate = document.getElementById("birthdate");
-const birthdateError = document.getElementById("birthdateError");
+const errorBirthdate = document.getElementById("errorBirthdate");
 const subscriptionStatus = document.getElementById("subscriptionStatus");
+const btnSaveChanges = document.getElementById("btnSaveChanges")
 
-firstName.addEventListener("focusout", (event) => {
-    if (firstName.value == ""){
-        event.target.style.color = "red"
-        firstName.value = "Feld darf nicht leer sein"
+
+
+firstName.addEventListener("focusout", () => {
+    if (firstName.value == ""){        
+        errorFirstName.innerText = "Feld darf nicht leer sein";
+        btnSaveChanges.disabled = true;
+    } else { 
+        errorFirstName.innerText = "";
+        btnSaveChanges.disabled = false;
     }
 });
 
-lastName.addEventListener("focusout", (event) => {
+lastName.addEventListener("focusout", () => {
     if (lastName.value == ""){
-        event.target.style.color = "red"
-        lastName.value = "Feld darf nicht leer sein"
+        errorLastName.innerText = "Feld darf nicht leer sein";
+        btnSaveChanges.disabled = true;
+    } else {
+        errorLastName.innerText = "";
+        btnSaveChanges.disabled = false;
     }
 });
 
 birthdate.addEventListener("focusout", () => {
     let now = Date.now();
-    let inputDate = new Date(birthdate.value);
-    // if (birthdate.value > now) {
-    //     birthdateError.style.color = "red"
-    //     birthdateError.value = "Datum darf nicht in der Zukunft liegen"
-    // }
-    console.log(now);
-    console.log(inputDate);
+    let inputDate = new Date(birthdate.value).getTime();
+    
+    if (inputDate > now) {
+        errorBirthdate.innerText = "Datum darf nicht in der Zukunft liegen";
+        btnSaveChanges.disabled = true;
+    } else {
+        errorBirthdate.innerText = "";
+        btnSaveChanges.disabled = false;
+    }   
 });
 
 subscriptionStatus.addEventListener("change", () => {
